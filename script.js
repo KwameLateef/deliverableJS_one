9214341597f17aa75453cc2e0c19a4638e917fda
Kwame Hampton <khamp1st@gmail.com>
2018-03-16 08:39:08 -0400

    Second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var input=prompt("ENTER A NUMBER")
+
+var output=0
+for (var counter=1; counter<=input; counter++){
+    console.log ("counter:",counter);
+    output+=counter;
+    console.log("output:",output);
+
+} 
